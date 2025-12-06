f1fa5dfbaba9721ffb9c143cdafcd0afa57dc71e
Norm <normandy@biribiri.dev>
2025-12-06 15:44:06 -0500

    Format main output

--- src/main/java/dev/biribiri/Main.java
+++ src/main/java/dev/biribiri/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
     static void main() {
         try {
+            IO.println("== Day 1 ==");
             Day1.run("src/main/resources/input-day-1");
         } catch (IOException e) {
             throw new RuntimeException(e);
