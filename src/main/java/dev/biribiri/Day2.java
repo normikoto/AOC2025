c098a33fd1350c8b79b2482e6be7c9e8204f7d5d
Norm <normandy@biribiri.dev>
2025-12-07 23:19:42 -0500

    Day 2

---
+++ src/main/java/dev/biribiri/Day2.java
@@ -0,0 +1,46 @@
+package dev.biribiri;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.stream.LongStream;
+
+public class Day2 {
+    public static void run(String inputFile) throws IOException {
+        long part1 = 0;
+        long part2 = 0;
+        try (var reader = new BufferedReader(new FileReader(inputFile))) {
+            String input = reader.readLine();
+            String[] ranges = input.split(",");
+            for (String range : ranges) {
+                String[] rangeParts = range.split("-");
+                long start = Long.parseLong(rangeParts[0]);
+                long end = Long.parseLong(rangeParts[1]);
+
+                part1 += LongStream.range(start, end + 1).filter(l -> {
+                    String longStr = Long.toString(l);
+                    String first = longStr.substring(0, longStr.length() / 2);
+                    String second = longStr.substring(longStr.length() / 2);
+
+                    return first.equals(second);
+                }).sum();
+
+                part2 += LongStream.range(start, end + 1).filter(l -> {
+                    String longStr = Long.toString(l);
+
+                    for (int n = 2; n <= longStr.length(); n++) {
+                        String prefix = longStr.substring(0, longStr.length() / n);
+                        if (prefix.repeat(n).equals(longStr)) {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }).sum();
+            }
+        }
+
+        IO.println("Part 1: " + part1);
+        IO.println("Part 2: " + part2);
+    }
+}
--- src/main/java/dev/biribiri/Main.java
+++ src/main/java/dev/biribiri/Main.java
@@ -7,6 +7,9 @@ public class Main {
         try {
             IO.println("== Day 1 ==");
             Day1.run("src/main/resources/input-day-1");
+
+            IO.println("== Day 2 ==");
+            Day2.run("src/main/resources/input-day-2");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
