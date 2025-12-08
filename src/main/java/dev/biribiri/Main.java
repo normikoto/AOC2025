2026914f49974bfc42c152136b53e482e1547f1c
Norm <normandy@biribiri.dev>
2025-12-08 12:04:42 -0500

    Replace try/catch with throwing IOException from main

--- src/main/java/dev/biribiri/Main.java
+++ src/main/java/dev/biribiri/Main.java
@@ -3,15 +3,13 @@ package dev.biribiri;
 import java.io.IOException;
 
 public class Main {
-    static void main() {
-        try {
-            IO.println("== Day 1 ==");
-            Day1.run("src/main/resources/input-day-1");
+    static void main() throws IOException {
+        IO.println("== Day 1 ==");
+        Day1.run("src/main/resources/input-day-1");
 
-            IO.println("== Day 2 ==");
-            Day2.run("src/main/resources/input-day-2");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        IO.println("== Day 2 ==");
+        Day2.run("src/main/resources/input-day-2");
+
+        IO.println("== Day 3 ==");
     }
 }
